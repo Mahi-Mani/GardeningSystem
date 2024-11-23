8034c920edd3df2e99476e51d1b45910d06c627b
Mahi-Mani <m.mahishamani@gmail.com>
2024-11-22 16:18:02 -0800

    Implement sprinkler system

--- GardenSimulation/src/main/java/com/example/gardensimulation/SprinklerController.java
+++ GardenSimulation/src/main/java/com/example/gardensimulation/SprinklerController.java
@@ -1,14 +1,26 @@
 package com.example.gardensimulation;
 
 import com.example.gardensimulation.Plant.Plants;
+import java.util.logging.*;
 
 import java.util.ArrayList;
 
 public class SprinklerController {
+    private static final Logger log = Logger.getLogger(SprinklerController.class.getName());
+
     public void activateSprinklers(ArrayList<Plants> plants) {
         int avgWaterReq = calculateWaterForGarden(plants);
-        for(Plants plant: plants) {
-            plant.waterThePlant(avgWaterReq);
+        int avgWaterLevel = calculateCurrentWaterLevel(plants);
+        log.info("Water requirement: " + avgWaterReq);
+        log.info("Current water level: " + avgWaterLevel);
+
+        if(avgWaterLevel < avgWaterReq/2) {
+            for(Plants plant: plants) {
+                plant.waterThePlant(avgWaterReq);
+            }
+            log.info("Sprinklers activated! Plants received an average of " + avgWaterReq + " units.");
+        } else {
+            log.info("Sufficient water in the garden already! No need sprinklers!");
         }
     }
 
@@ -16,6 +28,15 @@ public class SprinklerController {
         int total = 0;
         for(Plants plant: plants) {
             total = total + plant.getWater_requirement();
+        }
+        int average = total / plants.size();
+        return average;
+    }
+
+    private int calculateCurrentWaterLevel(ArrayList<Plants> plants) {
+        int total = 0;
+        for(Plants plant: plants) {
+            total = total + plant.getWater_level();
         }
         int average = total / plants.size();
         return average;
